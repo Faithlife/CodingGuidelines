5af2b90ee795770302e9545fc366eddca852eab5
Ed Ball <ed.ball@faithlife.com>
2020-07-09 12:17:41 -0700

    Update standard editorconfig.

--- sections/csharp/files/UpdateEditorConfig.ps1
+++ sections/csharp/files/UpdateEditorConfig.ps1
@@ -1,4 +1,4 @@
 $text = [System.IO.File]::ReadAllText("$PSScriptRoot\..\editorconfig.md")
-$code = [regex]::Split(-join [regex]::Matches($text, '```\s*(.*?)```', 'SingleLine').foreach({$_.Groups[1].Value}), '\r?\n')
+$code = [regex]::Split(-join [regex]::Matches($text, '```editorconfig\s*(.*?)```', 'SingleLine').foreach({$_.Groups[1].Value}), '\r?\n')
 $code = $code[0..2] + ($code[3..($code.Length - 1)] | Where-Object { $_ -ne '' } | Sort-Object)
 [System.IO.File]::WriteAllText("$PSScriptRoot\.editorconfig", ($code -join "`n") + "`n")
