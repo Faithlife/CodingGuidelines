569347b9d9066a3f81e6eebe61d5bbad8968df90
Dave Dunkin <dave.dunkin@logos.com>
2021-05-28 11:31:49 -0700

    Use join-path to allow running on mac.

--- sections/csharp/files/UpdateEditorConfig.ps1
+++ sections/csharp/files/UpdateEditorConfig.ps1
@@ -1,4 +1,4 @@
-$text = [System.IO.File]::ReadAllText("$PSScriptRoot\..\editorconfig.md")
+$text = [System.IO.File]::ReadAllText((Join-Path $PSScriptRoot ".." "editorconfig.md"))
 $code = [regex]::Split(-join [regex]::Matches($text, '```editorconfig\s*(.*?)```', 'SingleLine').foreach({$_.Groups[1].Value}), '\r?\n')
 $code = $code[0..2] + ($code[3..($code.Length - 1)] | Where-Object { $_ -ne '' } | Sort-Object)
-[System.IO.File]::WriteAllText("$PSScriptRoot\.editorconfig", ($code -join "`n") + "`n")
+[System.IO.File]::WriteAllText((Join-Path $PSScriptRoot ".editorconfig"), ($code -join "`n") + "`n")
